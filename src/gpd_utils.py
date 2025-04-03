4ea01ba2e0b5c3e33b69317bb27c7389d3318fc8
Colladito <victorcollado1@gmail.com>
2025-04-03 12:33:14 +0200

    Changes in dq_gpd an add analytical hessian

--- src/gpd_utils.py
+++ src/gpd_utils.py
@@ -31,13 +31,14 @@ def dq_gpd(F, u, sigma, gamma):
     # Gumbel case
     if np.abs(gamma) < 1e-8:
         Dqu = np.zeros(len(F))
-        Dqsigma = -np.log(1-F)
+        Dqsigma = -np.log(1-np.log(F))
         Dqxi = np.zeros(len(F))
     # General case
     else:
         Dqu = np.zeros(len(F))
         Dqsigma = -(1-(1-F)**(-gamma))/gamma
-        Dqxi = sigma*(1-(1-F)**(-gamma)*(1+gamma*np.log(1-F)))/(gamma**2)
+        Dqxi = -(sigma/((gamma**2)*(1-F)**(gamma)))*(gamma*np.log(1-F)-(1-F)**(gamma)+1)
+        # Dqxi = sigma*(1-(1-F)**(-gamma)*(1+gamma*np.log(1-F)))/(gamma**2)
 
     Dq[0,:] = Dqu
     Dq[1,:] = Dqsigma*sigma
@@ -75,3 +76,63 @@ def nll_gpd(data, p):
         expr = 1+xi*((data-u)/sigma)
         expr = np.maximum(expr, 1e-5)
         return (N*np.log(sigma) + (1+1/xi)*np.sum(np.log(expr)))
+    
+
+def nll_gpd_hessian(data, p):
+    """
+    Negative Loglikelihood of Stationary GPD distribution.
+    Usefull to compute the variance-covariance matrix using the hessian from numdifftools
+
+    Args:
+        data (_type_): data
+        p (list): parameters of GPD distribution [threshold, scale, shape] 
+
+    Returns:
+        loglikelihood (np.array): negative loglikelihood value
+    """
+
+    u = p[0]       # Location
+    sigma = p[1]   # Scale
+    xi = p[2]      # Shape
+
+    N = len(data)
+    
+    # Gumbel 
+    if np.abs(xi) < 1e-8:
+        expr = (data-u)/sigma
+        expr = np.maximum(expr, 1e-5)
+        f = (N*np.log(sigma) + np.sum(expr))
+
+    # Weibull-Frechet 
+    else:
+        expr = 1+xi*((data-u)/sigma)
+        expr = np.maximum(expr, 1e-5)
+        f = (N*np.log(sigma) + (1+1/xi)*np.sum(np.log(expr)))
+
+
+    # Jacobian
+    j = np.zeros(3)
+    z = (data-u)/sigma
+    expr = 1+xi*z
+    j[0] = -(1+1/xi)*np.sum(xi/(sigma*expr))
+    j[1] = N/sigma - (1+1/xi)*np.sum(xi*z/(sigma*expr))
+    j[2] = -1/(xi**2)*np.sum(np.log(expr)) + (1+1/xi)*np.sum(z/expr)
+
+    # Hessian
+    hess = np.zeros((3,3))
+    # Diagonal elements
+    hess[0,0] = (1+1/xi)*np.sum(xi**2/((sigma**2)*(expr**2)))
+    hess[1,1] = -N/sigma**2 + (1+1/xi)*np.sum(2*xi*z/(sigma**2*expr)- xi**2*z**2/(sigma**2*expr**2))
+    hess[2,2] = 2/(xi**3)*np.sum(np.log(expr))-2/(xi**2)*np.sum(z/expr)-(1+1/xi)*np.sum(z**2/(expr**2))
+
+    # Cross 
+    hess[0,1] = (1+1/xi)*np.sum(xi**2*z/(sigma**2*expr**2))
+    hess[1,0] = (1+1/xi)*np.sum(xi**2*z/(sigma**2*expr**2))
+    
+    hess[0,2] = 1/(xi**2)*np.sum(1/expr)-(1+1/xi)*np.sum(z/(sigma*expr**2))
+    hess[2,0] = 1/(xi**2)*np.sum(1/expr)-(1+1/xi)*np.sum(z/(sigma*expr**2))
+
+    hess[1,2] = (1+1/xi)*np.sum(z/expr**2*(xi*z/sigma)) - 1/(xi**2)*np.sum(xi*z/(sigma*expr))
+    hess[2,1] = (1+1/xi)*np.sum(z/expr**2*(xi*z/sigma)) - 1/(xi**2)*np.sum(xi*z/(sigma*expr))
+    
+    return f, j, hess
